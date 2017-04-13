df889d238dfa9eaad9f65758b29db171846bd740
LGBSPL\ddziedzinski <Dariusz.Dziedzinski@euvic.pl>
2017-04-13 15:45:52 +0200

    :)

---
+++ Toci.EntityAnalyzer.Interfaces/Toci.EntityAnalyzer.Interfaces/Entities/IClassCode.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Toci.EntityAnalyzer.Interfaces.Entities
+{
+    public interface IClassCode
+    {
+        List<string> Consts { get; set; }
+        List<string> Fields { get; set; }
+        List<string> Properties { get; set; }
+        List<string> Methods { get; set; }
+    }
+}
