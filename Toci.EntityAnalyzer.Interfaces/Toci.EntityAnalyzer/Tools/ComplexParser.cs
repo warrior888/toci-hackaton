716c70eba57e32d773758f5e97de90a54c4f3fd0
Cienia <cienia.root@gmail.com>
2017-04-15 17:08:29 +0200

    ComplexParser ParseEntireCt - up for discussion

--- Toci.EntityAnalyzer.Interfaces/Toci.EntityAnalyzer/Tools/ComplexParser.cs
+++ Toci.EntityAnalyzer.Interfaces/Toci.EntityAnalyzer/Tools/ComplexParser.cs
@@ -1,4 +1,6 @@
-﻿using Toci.EntityAnalyzer.Interfaces.Entities;
+﻿using System;
+using System.Linq;
+using Toci.EntityAnalyzer.Interfaces.Entities;
 using Toci.EntityAnalyzer.Interfaces.Tools;
 
 namespace Toci.EntityAnalyzer.Tools
@@ -7,7 +9,24 @@ namespace Toci.EntityAnalyzer.Tools
     {
         public virtual IComplexEntity ParseEntireCt(string tableCreates)
         {
-            return null; // TODO
+            var baseType = typeof(ComplexParser<TResult>);
+            var assembly = baseType.Assembly;
+
+            var complexParsers = assembly.GetTypes().Where(t => t.IsSubclassOf(baseType));
+            foreach (var parser in complexParsers)
+            {
+                if(parser != null)
+                {
+                    dynamic instance = Activator.CreateInstance(parser);
+                    var method = instance.GetMethod("Parse");
+                    if (method != null)
+                    {
+                        method.Invoke(instance, tableCreates);
+                    }
+                }
+               
+            }
+            return null; // TODO          
         }
 
         protected abstract TResult Parse(string tableCreates);
