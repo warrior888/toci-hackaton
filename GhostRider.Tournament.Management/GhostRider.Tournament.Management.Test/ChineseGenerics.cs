fd081040fb8f33910ac75d9c737b202605e6735f
Bartlomiej Zapart <bartlomiej.zapart@euvic.pl>
2017-04-22 13:11:47 +0200

    co and contra variance poc.

---
+++ GhostRider.Tournament.Management/GhostRider.Tournament.Management.Test/ChineseGenerics.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Euvic.Generator.Core.Test.Poc
+{
+    [TestClass]
+    public class ChineseGenerics
+    {
+        [TestMethod]
+        public void Test()
+        {
+            BusinessLogic bl = new BusinessLogic();
+
+            bl.DoSomething(new AnotherWhatever()); //BaseWhatever
+            bl.DoSomething(new BaseWhatever()); //BaseWhatever
+        }
+    }
+
+    abstract class BusinessLogicBase
+    {
+        public virtual void DoSomething(IBaseType<AGenericType> parameter)
+        {
+
+        }
+    }
+
+    class BusinessLogic : BusinessLogicBase
+    {
+        public void DoSomething(IBaseType<BGenericType> parameter)
+        {
+            DoSomething((IBaseType<AGenericType>)parameter);
+        }
+    }
+
+    interface AGenericType { }
+
+    interface BGenericType : AGenericType { }
+
+    class BaseWhatever : IBaseType<AGenericType> { }
+
+
+    class AnotherWhatever : IBaseType<BGenericType> { }
+
+    interface IBaseType<TResult> { }
+
+}
