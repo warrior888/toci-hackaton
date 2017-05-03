458431d05ea6d636de55191f77ca4bf5a9db062c
warrior888 <warriorr@poczta.fm>
2017-05-03 23:19:31 +0200

    no message

---
+++ GhostRider.Tournament.Management/GhostRider.Tournament.Management.Test/CoContra.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostRider.Tournament.Management.Test
+{
+    class CoContra
+    {
+        public void test()
+        {
+            //IRainyWeather<IRoman> bla = new RainyWeather<ICloud>();
+        }
+    }
+
+    //PLCHANDLER<PLCCOMMAND>
+
+    internal interface ICloud
+    {
+    }
+
+    interface ISpecificCloud : ICloud
+    {
+
+       
+    }
+
+    interface IRoman : ISpecificCloud
+    {
+
+    }
+
+    interface IWeather<out TCloud> where TCloud : ICloud
+    {
+        TCloud DoWhatever();
+    }
+
+    interface ICoolWeather<out TCloud> : IWeather<TCloud> where TCloud : ICloud { }
+
+    interface IRainyWeather<out TCloud> : ICoolWeather<TCloud> where TCloud : ICloud { }
+
+    class Roman : IRoman { }
+    class RainyWeather<TCloud> : IRainyWeather<TCloud> where TCloud : ICloud {
+        public TCloud DoWhatever()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/GhostRider/Tekken/Main/TournamentMainWindow.java
@@ -0,0 +1,13 @@
+package GhostRider.Tekken.Main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TournamentMainWindow extends Application {
+
+	public void start(Stage arg0) throws Exception {
+		
+		//launch(arg0);
+	}
+
+}
