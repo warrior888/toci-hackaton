7a195937b326e8a61b69a55cb283a8577361ebef
Erley <kopczykf@gmail.com>
2017-05-23 10:32:09 +0200

    Reflection Test

---
+++ Toci.EntityAnalyzer.Interfaces/Toci.Generator.Test/Toci.EntityAnalyzer.Tests/Reflection/ReflectionTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.EntityAnalyzer.Interfaces.Entities;
+
+namespace Toci.Generator.Test.Toci.EntityAnalyzer.Tests.Reflection
+{
+    [TestClass]
+    public class ReflectionTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //var results = from type in Toci.EntityAnalyzer.
+            //              where typeof(I).IsAssignableFrom(type)
+            //              select type;
+
+            IEnumerable<Type> mytypes =
+            System.Reflection.Assembly.GetExecutingAssembly()
+                .GetTypes()
+                .Where(mytype => mytype.GetInterfaces().Contains(typeof(IParseResult)));
+            
+
+        }
+    }
+}
